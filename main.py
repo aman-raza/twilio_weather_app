c34b3dba00f561c8366889c20efa8432691e6d03
aman <53443872+aman-raza@users.noreply.github.com>
2024-02-13 02:59:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from twilio.rest import Client
 
 MY_LAT = 32.080898
 MT_LONG = -81.091202
-API_KEY = "bfbd05baca9a4d02d0159c62ae9b978a" # os.environ.get("API_KEY")
-account_sid = "AC63f919d11afd5c70ffc6f27c3bf39e15"
-auth_token = "bb7b95390da997ff37c04635842bbffe" # os.environ.get("AUTH_TOKEN")
+API_KEY = ".....5baca9a4d02d0159c62ae9....." # os.environ.get("API_KEY")
+account_sid = "AC63f919d11afd.....fc6f27c3bf39e15"
+auth_token = ".....5390da997ff37c04635842....." # os.environ.get("AUTH_TOKEN")
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
